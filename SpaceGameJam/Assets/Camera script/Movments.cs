1d3565ec5f6f95a2354cc65c5d0273d6ef44a81e
Fateh <fateh.alchhabi@bouvet.no>
2021-10-22 23:16:41 +0200

    spaceship movement with A,D

---
+++ SpaceGameJam/Assets/Camera script/Movments.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movments : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;	
+    public float sidewaysForce = 500f;  
+    void FixedUpdate ()
+    {
+        // rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))	
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))  
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
