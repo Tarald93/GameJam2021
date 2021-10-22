a43bfce305f065a184ee777a1904082dfa10c48f
SimonBay <Simon_bay@hotmail.com>
2021-10-23 00:17:20 +0200

    updated movements script with camera limits

--- SpaceGameJam/Assets/Camera script/Movments.cs
+++ SpaceGameJam/Assets/Camera script/Movments.cs
@@ -7,19 +7,36 @@ public class Movments : MonoBehaviour
     public Rigidbody rb;
 
     public float forwardForce = 2000f;	
-    public float sidewaysForce = 500f;  
+    public float sidewaysForce = 500f;
+    public float maxTravelDistance = 100f;
     void FixedUpdate ()
     {
         // rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
+        if (transform.position.x > (maxTravelDistance - 0.01f) && rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.y, 0, 0);
+        }
+        if (transform.position.x < -(maxTravelDistance - 0.01f) && rb.velocity.x < 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.y, 0, 0);
+        }
+
         if (Input.GetKey("d"))	
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            if (transform.position.x < (maxTravelDistance - 0.01f))
+            {
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            }
+            
         }
 
         if (Input.GetKey("a"))  
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            if (transform.position.x > -(maxTravelDistance - 0.01f))
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            }
         }
     }
 }
